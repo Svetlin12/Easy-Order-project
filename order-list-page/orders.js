be7063973f54c54da6286548952b911471196471
Svetlin12 <svetlinip@gmail.com>
2021-05-20 14:25:32 +0300

    Fixed orders tab and a typo in dish-page.html

--- order-list-page/orders.js
+++ order-list-page/orders.js
@@ -1,42 +1,83 @@
-$(document).ready(function () {
-    $(document).on("scroll", onScroll);
+const saladsLink = document.getElementById("salads-link");
+const pizzaLink = document.getElementById("pizza-link");
+const appetizerLink = document.getElementById("appetizer-link");
+const dessertLink = document.getElementById("dessert-link");
+const drinksLink = document.getElementById("drinks-link");
+const menuList = document.getElementById("menu-list");
+var lastScrollPos = 0;
 
-    //smoothscroll
-    $('a[href^="#"]').on('click', function (e) {
-        e.preventDefault();
-        $(document).off("scroll");
+$(menuList).ready(function() {
+    var scroll_pos = 0;
+    $(menuList).scroll(function() {
+      scroll_pos = $(this).scrollTop();
 
-        $('a').each(function () {
-            $(this).removeClass('active');
-        })
-        $(this).addClass('active');
+      var saladsThreshold;
+      var pizzaThreshold;
+      var appetizerThreshold;
+      var dessertThreshold;
 
-        var target = this.hash,
-            menu = target;
-        $target = $(target);
-        $('html, body').stop().animate({
-            'scrollTop': $target.offset().top + 2
-        }, 500, 'swing', function () {
-            window.location.hash = target;
-            $(document).on("scroll", onScroll);
-        });
+      if (scroll_pos > lastScrollPos) {
+        saladsThreshold = 246;
+        pizzaThreshold = 739;
+        appetizerThreshold = 1109;
+        dessertThreshold = 1210;
+      }
+      else {
+          saladsThreshold = 123;
+          pizzaThreshold = 493;
+          appetizerThreshold = 862;
+          dessertThreshold = 1134;
+      }
+
+      if (scroll_pos < saladsThreshold) {
+        saladsLink.classList.add("active");
+        pizzaLink.classList.remove("active");
+        appetizerLink.classList.remove("active");
+        dessertLink.classList.remove("active");
+        drinksLink.classList.remove("active");
+        lastScrollPos = scroll_pos;
+        return;
+      }
+
+      if (scroll_pos < pizzaThreshold) {
+        saladsLink.classList.remove("active");
+        pizzaLink.classList.add("active");
+        appetizerLink.classList.remove("active");
+        dessertLink.classList.remove("active");
+        drinksLink.classList.remove("active");
+        lastScrollPos = scroll_pos;
+        return;
+      }
+
+      if (scroll_pos < appetizerThreshold) {
+        saladsLink.classList.remove("active");
+        pizzaLink.classList.remove("active");
+        appetizerLink.classList.add("active");
+        dessertLink.classList.remove("active");
+        drinksLink.classList.remove("active");
+        lastScrollPos = scroll_pos;
+        return;
+      }
+
+      if (scroll_pos < dessertThreshold) {
+        saladsLink.classList.remove("active");
+        pizzaLink.classList.remove("active");
+        appetizerLink.classList.remove("active");
+        dessertLink.classList.add("active");
+        drinksLink.classList.remove("active");
+        lastScrollPos = scroll_pos;
+        return;
+      }
+
+      saladsLink.classList.remove("active");
+      pizzaLink.classList.remove("active");
+      appetizerLink.classList.remove("active");
+      dessertLink.classList.remove("active");
+      drinksLink.classList.add("active");
+      lastScrollPos = scroll_pos;
+      return;
     });
-});
-
-function onScroll(event) {
-    var scrollPos = $(document).scrollTop();
-    $('#menu-center a').each(function () {
-        var currLink = $(this);
-        var refElement = $(currLink.attr("href"));
-        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
-            $('#menu-center ul li a').removeClass("active");
-            currLink.addClass("active");
-        }
-        else {
-            currLink.removeClass("active");
-        }
-    });
-}
+  });
 
 function addDishesCard(type, name, price, img, func) {
     var cardTitle = document.createElement("h3");
