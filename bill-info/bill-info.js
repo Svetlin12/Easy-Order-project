4898f86ae47f070a634d4dbcb4c28e063e948ab7
Svetlin12 <svetlinip@gmail.com>
2021-05-21 13:53:30 +0300

    Fixed dishes pages. Added confirm promp upon product removal. Minor updates as well.

--- bill-info/bill-info.js
+++ bill-info/bill-info.js
@@ -6,7 +6,7 @@ function addDish(i, dish) {
     dishName.appendChild(text);
 
     var priceText = document.createElement("h2");
-    text = document.createTextNode("Цена");
+    text = document.createTextNode("Цена:");
     priceText.appendChild(text);
 
     var priceNumber = document.createElement("h2");
@@ -51,17 +51,42 @@ function addDish(i, dish) {
 }
 
 function removeDish(i) {
-    console.log(i)
-    var current = parseFloat(document.getElementById("new-bill").innerText);
-    var discount = parseFloat(dishList[i].price);;
-    console.log(current - discount);
-    setSums(current - discount);
-    document.getElementById('dishes-list').removeChild(document.getElementById(i));
-    delete dishList[i];
-    localStorage.setItem('card', JSON.stringify(dishList));
+    const waitForUserInput = new Promise((resolve, reject) => {
+        var isUserSure = null;
+        document.getElementById("confirm-message-container").classList.add("visible");
+        document.getElementById("dishes-list").classList.add("hide-scrollbar");
+    
+        const confirmButton = document.getElementById("confirm-button");
+        const denyButton = document.getElementById("deny-button");
+    
+        // invoke only once
+        confirmButton.addEventListener('click', () => {
+            isUserSure = true;
+            resolve(isUserSure);
+        }, {once: true});
+        
+        // invoke only once
+        denyButton.addEventListener('click', () => {
+            isUserSure = false;
+            resolve(isUserSure);
+        }, {once: true});
+    })
 
+    waitForUserInput.then((response) => {
+        document.getElementById("confirm-message-container").classList.remove("visible");
+        document.getElementById("dishes-list").classList.remove("hide-scrollbar");
+        if (response == false) {
+            return;
+        }
+
+        var current = parseFloat(document.getElementById("new-bill").innerText);
+        var discount = parseFloat(dishList[i].price);;
+        setSums(current - discount);
+        document.getElementById('dishes-list').removeChild(document.getElementById(i));
+        delete dishList[i];
+        localStorage.setItem('card', JSON.stringify(dishList));
+    })
 }
-
 
 
 function fillCard() {
@@ -84,7 +109,15 @@ function fillCard() {
 function doOrder() {
     localStorage.setItem("finalBill", document.getElementById("final").innerText);
     localStorage.setItem('card', "[]");
+
     var list = document.getElementById('dishes-list')
+
+    var areThereProducts = true;
+    if (list.childElementCount == 0) {
+        areThereProducts = false;
+    }
+
+
     while (list.lastElementChild) {
         list.removeChild(list.lastElementChild);
     }
@@ -93,8 +126,26 @@ function doOrder() {
     document.getElementById("current-bill").innerText = document.getElementById("final").innerText;
     document.getElementById("new-bill").innerText = startingPrice.toFixed(2);
     document.getElementById("discount").innerText = startingPrice.toFixed(2);
-}
 
+    if (areThereProducts) {
+        const responseDiv = document.getElementById("response");
+        responseDiv.classList.add("show-popup");
+    
+        // waits untill all animations have finished, then removes the class from the class list
+        Promise.all(
+            responseDiv.getAnimations().map(
+                function(animation) {
+                    return animation.finished;
+                }
+            )
+        )
+        .then(
+            function() {
+                responseDiv.classList.remove("show-popup");
+            }
+        )
+    }
+}
 
 function setSums(price) {
     document.getElementById("new-bill").innerText = price.toFixed(2);
