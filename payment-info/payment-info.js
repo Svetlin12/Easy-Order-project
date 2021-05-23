72e7f8bd5172124f0fd271ccfbeb0be27708ec3b
Svetlin12 <svetlin_popivanov@abv.bg>
2021-05-23 12:32:34 +0300

    Add pop-up message upon clicking "В брой" button.

--- payment-info/payment-info.js
+++ payment-info/payment-info.js
@@ -1,3 +1,32 @@
 function fillCurrentBill() {
-    document.getElementById("current-bill").innerText = localStorage.getItem("finalBill");
+    const currentBillField = document.getElementById("current-bill");
+    currentBillField.innerText = localStorage.getItem("finalBill");
+
+    const cashButton = document.getElementById("cash");
+
+    cashButton.addEventListener("click", () => {
+        if (currentBillField.innerText == "0.00") {
+            return;
+        }
+
+        currentBillField.innerText = "0.00";
+        localStorage.setItem("finalBill", "0.00");
+        
+        const responseDiv = document.getElementById("response");
+        responseDiv.classList.add("show-popup");
+
+        // waits untill all animations have finished, then removes the class from the class list
+        Promise.all(
+            responseDiv.getAnimations().map(
+                function(animation) {
+                    return animation.finished;
+                }
+            )
+        )
+        .then(
+            function() {
+                responseDiv.classList.remove("show-popup");
+            }
+        )
+    });
 }
